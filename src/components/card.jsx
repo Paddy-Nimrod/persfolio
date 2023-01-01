135425effd4b443be33f8c3fded2f0f4bb412d61
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-01-02 00:43:52 +0300

    added new skills feature on home page

---
+++ src/components/card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CardComponent = () => {
+  return (
+    <div className="w-9 bg-blue-200">
+      <div>
+        <ul>
+          <li>Java</li>
+          <li>Spring Boot</li>
+          <li>Java</li>
+          <li>Spring Boot</li>
+          <li>Java</li>
+          <li>Spring Boot</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CardComponent;
--- src/components/skillcomponent.jsx
+++ src/components/skillcomponent.jsx
@@ -1,16 +1,12 @@
 import React from "react";
+import CardComponent from "./card";
 
 const SkillComponent = () => {
   return (
-    <div className="flex items-center bg-slate-500 w-full">
-      <ul>
-        <li>Java</li>
-        <li>Spring Boot</li>
-        <li>Java</li>
-        <li>Spring Boot</li>
-        <li>Java</li>
-        <li>Spring Boot</li>
-      </ul>
+    <div className="grid grid-cols-3 gap-1 items-center bg-slate-500 w-full">
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
     </div>
   );
 };
