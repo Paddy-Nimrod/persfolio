d023ae58e672bc538c0e6e40260de69f2ac61853
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-01-26 14:01:08 +0300

    added new features to the design

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,17 +2,45 @@ import React from "react";
 
 const CardComponent = () => {
   return (
-    <div className="card bg-blue-200">
-      <div className="p-4 items-center">
-        <h2>Technology</h2>
-        <ul>
-          <li>Java</li>
-          <li>Spring Boot</li>
-          <li>Java</li>
-          <li>Spring Boot</li>
-          <li>Java</li>
-          <li>Spring Boot</li>
-        </ul>
+    <div className="grid grid-cols-1 gap md:grid-cols-3 lg:grid-cols-3 w-full my-4">
+      <div className="card bg-slate-900 text-white">
+        <div className="p-4 items-center">
+          <h2 className=" text-lg font-bold m-2 ">Technology</h2>
+          <ul>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>Java</li>
+            <li>Spring Boot</li>
+          </ul>
+        </div>
+      </div>
+      <div className="card bg-slate-900 text-white">
+        <div className="p-4 items-center">
+          <h2>Technology</h2>
+          <ul>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>Java</li>
+            <li>Spring Boot</li>
+          </ul>
+        </div>
+      </div>
+      <div className="card bg-slate-900 text-white">
+        <div className="p-4 items-center">
+          <h2>Technology</h2>
+          <ul>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>Java</li>
+            <li>Spring Boot</li>
+            <li>Java</li>
+            <li>Spring Boot</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/skillcomponent.jsx
+++ src/components/skillcomponent.jsx
@@ -4,9 +4,7 @@ import CardComponent from "./card";
 const SkillComponent = () => {
   return (
     <div className="card-container flex items-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 items-center justify-center p-4 w-full">
-        <CardComponent />
-        <CardComponent />
+      <div className="flex p-4 w-full">
         <CardComponent />
       </div>
     </div>
