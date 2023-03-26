b41b50a37f8a17dab525d3ce3dd82b7d177079a4
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-03-26 06:34:28 +0300

    UI enhancements

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -7,7 +7,10 @@ const HomeComponent = () => {
     <div className=" font-dinregular">
       <div className="home-container flex flex-col md:flex-row lg:flex-row py-4">
         <div className="text-white capitalize md:text-lg lg:text-lg top-1/4 md:w-1/3 lg:w-1/3 px-4 font-bold  m-auto">
-          <h1>Welcome to nimrods tech space</h1>
+          <div className="font-bold text-4xl">Cicenic Technologies.</div>
+          <div className="font-dinregular p-4">
+            "Good software, like wine, takes time."
+          </div>
         </div>
         <div className="text-white text-lg top-1/4 px-2 font-bold  md:w-1/2 lg:w-1/2 md:m-auto lg:m-auto my-2">
           <img
