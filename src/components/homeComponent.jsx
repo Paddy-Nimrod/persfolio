8c36dc2ba9d0cdbe800c7c4057a62e41aafc2d86
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-04-05 09:04:03 +0300

    added new features

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -7,7 +7,7 @@ const HomeComponent = () => {
     <div className=" font-dinregular">
       <div className="home-container flex flex-col md:flex-row lg:flex-row py-4">
         <div className="text-white capitalize md:text-lg lg:text-lg top-1/4 md:w-1/3 lg:w-1/3 px-4 font-bold  m-auto">
-          <div className="font-bold text-4xl">Cicenic Technologies.</div>
+          <div className="font-bold text-4xl">Nimrod Ambetsa.</div>
           <div className="font-dinregular p-4">
             "Good software, like wine, takes time."
           </div>
