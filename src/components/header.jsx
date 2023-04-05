0b4a06cc9422edd29117b85fcf9eaefc9745331a
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-04-05 17:18:12 +0300

    added navigation features

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const HeaderComponent = () => {
   return (
