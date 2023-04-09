cff9cb9075e3e195d61225e22b7cd91d7d6e507b
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-04-09 23:14:27 +0300

    added new feature on the services component

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div>
+      <h2>Services</h2>
+      <div>this are the services which we offer</div>
+    </div>
+  );
 }
 
 export default Services;
