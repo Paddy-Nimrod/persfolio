baeebd9caa80b2d07bbf0fe2645454f9db9be826
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-04-14 17:38:49 +0300

    added new features

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css";
-import homeImage from "../assets/images/home-img.jpg";
+import homeImage from "../assets/images/1.jpg";
 
 const HomeComponent = () => {
   return (
@@ -10,11 +10,11 @@ const HomeComponent = () => {
           <img
             src={homeImage}
             alt="web-design and development"
-            className="w-4/5 m-auto mix-blend-hard-light"
+            className=" w-1/3 m-auto rounded-full outline outline-offset-4 outline-2"
           />
+          <div className="font-normal text-xl m-4">Nimrod Ambetsa.</div>
         </div>
         <div className="text-white capitalize md:text-lg lg:text-lg top-1/4 md:w-1/3 lg:w-1/3 px-4 font-bold  m-auto">
-          <div className="font-bold text-4xl">Nimrod Ambetsa.</div>
           <div className="font-dinregular p-4">
             "Good software, like wine, takes time."
           </div>
