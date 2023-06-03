2edd961301ea63670322575c73117a381b5b08a4
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-06-03 17:14:34 +0300

    added new features on the contact page

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -14,12 +14,30 @@ function Contacts() {
   return (
     <div>
       <HeaderComponent />
-      <div className="grid grid-cols-1 items-center justify-center w-full m-auto h-screen">
-        <div className="grid grid-cols-2 gap-2 w-8/12 m-auto h-1/2 ">
-          <div className="h-14 bg-black w-1/2"> </div>
-          <div className="h-12 bg-black w-1/2"></div>
+      <div className="flex w-full m-auto h-screen p-4 ">
+        <div className="flex flex-col items-center justify-center w-6/12 m-auto h-5/6 p-4 gap-4">
+          <div className="h-2/4 flex bg-black w-8/12 rounded-md">
+            <div className="w-1/3 bg-white h-full"></div>
+            <div className="w-2/3 p-4">
+              {" "}
+              <div className="font-dinregular font-medium text-white">
+                hello client
+              </div>
+            </div>
+          </div>
+          <div className="h-2/4 flex bg-black w-8/12 rounded-md">
+            <div className="w-2/3 p-4">
+              <div className="font-dinregular font-medium text-white">
+                hello client
+              </div>
+            </div>
+            <div className="w-1/3 bg-white h-full"></div>
+          </div>
         </div>
-        <div className="w-11/12 m-auto">
+        <div className="w-6/12 m-auto h-5/6 p-4 gap-2">
+          <div className="m-4 font-dinregular font-medium capitalize">
+            Please leave your Message
+          </div>
           <Formik
             initialValues={{
               email: "",
@@ -31,25 +49,36 @@ function Contacts() {
             }}
           >
             {({ errors, touched }) => (
-              <Form className="flex flex-col gap-2 w-8/12 m-auto">
+              <Form className="flex flex-col gap-2 w-10/12 m-auto font-dinregular">
                 <div className="flex flex-col gap-2 w-full">
-                  <label className="font-medium">Email</label>
+                  <label className="font-semibold text-xs uppercase">
+                    Email
+                  </label>
+                  {errors.email && touched.email ? (
+                    <div>{errors.email}</div>
+                  ) : null}
                   <Field
-                    className="h-9 p-2 border-2 w-full"
+                    className="h-9 p-2 w-full rounded-sm border-none bg-slate-100"
                     name="email"
                     type="email"
                   />
                 </div>
-                <div className="flex flex-col gap-2">
-                  <label className="font-medium">Message</label>
+                <div className="flex flex-col gap-2 h-fit">
+                  <label className="font-semibold uppercase text-xs">
+                    Message
+                  </label>
                   <Field
-                    className="h-9 p-2 border-2"
+                    className="h-9 p-2 border-none rounded-sm"
                     name="message"
-                    type="textarea"
+                    as="textarea"
+                    rows="8"
                   />
                 </div>
                 <div className="flex justify-end">
-                  <button className="bg-black text-white items-end font-dinregular h-9 w-4/12 rounded-sm">
+                  <button
+                    type="submit"
+                    className="bg-black text-white items-end font-dinregular h-9 w-4/12 rounded-sm"
+                  >
                     Submit
                   </button>
                 </div>
