9d25c28b1613f6c83f611b55a69d9ac835671664
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-06-05 14:14:18 +0300

    added responsive contact page

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -3,7 +3,7 @@ import HeaderComponent from "../components/header";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const validationSchema = Yup.object().shape({
+const messageSchema = Yup.object().shape({
   Email: Yup.string().email("Invalid email address").required("Email Required"),
   message: Yup.string()
     .max(300, "Your message should be less than 300 characters")
@@ -14,28 +14,36 @@ function Contacts() {
   return (
     <div>
       <HeaderComponent />
-      <div className="flex w-full m-auto h-screen p-4 ">
-        <div className="flex flex-col items-center justify-center w-6/12 m-auto h-5/6 p-4 gap-4">
-          <div className="h-2/4 flex bg-black w-8/12 rounded-md">
+      <div className="md:flex w-full m-auto md:h-screen p-2 md:p-4">
+        <div className="grid grid-cols-1 w-full top-12 relative gap-4 md:flex md:flex-col md:items-center md:justify-center md:w-6/12 my-5 md:m-auto md:h-5/6 p-2 md:p-4 md:gap-4">
+          <div className=" h-20 md:h-2/4 flex bg-black w-full m-auto md:w-8/12 rounded-sm md:rounded-md">
             <div className="w-1/3 bg-white h-full"></div>
-            <div className="w-2/3 p-4">
-              {" "}
+            <div className="w-2/3 p-2 md:p-4">
               <div className="font-dinregular font-medium text-white">
                 hello client
               </div>
             </div>
           </div>
-          <div className="h-2/4 flex bg-black w-8/12 rounded-md">
+
+          <div className=" h-20 md:h-2/4 flex bg-black w-full m-auto md:w-8/12 rounded-sm md:rounded-md">
+            <div className="w-1/3 bg-white h-full"></div>
             <div className="w-2/3 p-4">
               <div className="font-dinregular font-medium text-white">
                 hello client
               </div>
             </div>
+          </div>
+          <div className=" h-20 md:h-2/4 flex bg-black w-full m-auto md:w-8/12 rounded-sm md:rounded-md border-none">
             <div className="w-1/3 bg-white h-full"></div>
+            <div className="w-2/3 p-4">
+              <div className="font-dinregular font-medium text-white">
+                hello client
+              </div>
+            </div>
           </div>
         </div>
-        <div className="w-6/12 m-auto h-5/6 p-4 gap-2">
-          <div className="m-4 font-dinregular font-medium capitalize">
+        <div className="w-full md:w-6/12 my-5 md:m-auto md:h-5/6 p-2 md:p-4 gap-2 relative">
+          <div className="m-2 md:m-4 font-dinregular font-medium capitalize">
             Please leave your Message
           </div>
           <Formik
@@ -43,9 +51,10 @@ function Contacts() {
               email: "",
               message: "",
             }}
-            validationSchema={validationSchema}
+            validationSchema={messageSchema}
             onSubmit={(values) => {
-              console.log(values);
+              console.log("submited");
+              alert(JSON.stringify(values));
             }}
           >
             {({ errors, touched }) => (
@@ -61,17 +70,19 @@ function Contacts() {
                     className="h-9 p-2 w-full rounded-sm border-none bg-slate-100"
                     name="email"
                     type="email"
+                    placeholder="email address"
                   />
                 </div>
-                <div className="flex flex-col gap-2 h-fit">
+                <div className=" flex flex-col gap-2">
                   <label className="font-semibold uppercase text-xs">
                     Message
                   </label>
                   <Field
-                    className="h-9 p-2 border-none rounded-sm"
+                    className="p-2 border-none rounded-sm bg-slate-100 resize-none"
                     name="message"
                     as="textarea"
-                    rows="8"
+                    rows="10"
+                    placeholder="message ..."
                   />
                 </div>
                 <div className="flex justify-end">
